7262db9656717b7ad71c5077e74777592f49374e
loricof23 <loricof42@gmail.com>
2021-04-07 14:32:29 +0700

    Add pagination dashboard
    Membuat pagination di dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -8,7 +8,7 @@
                 placeholder="Search"
                 addon-left-icon="ni ni-hat-3">
                 </base-input>
-               <base-button tag="a" href="#/formAddItem"
+               <base-button tag="a"
              class="mb-3 mb-sm-0">
              Search
              </base-button>
@@ -39,7 +39,27 @@
         </card>
       </div> -->
 
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span class="sr-only">Previous</span>
+            </a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
+    
 </template>
 <script>
 import { BaseTable } from "@/components";
