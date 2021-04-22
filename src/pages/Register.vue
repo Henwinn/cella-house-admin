29194edbe24ab6685917f6ad112194259e727481
loricof23 <loricof42@gmail.com>
2021-04-22 11:48:58 +0700

    Update data register form

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -19,38 +19,59 @@
                           class="border-0">
                         <template>
                             <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </base-button>
-
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </base-button>
+                                <small>Register</small>
                             </div>
                         </template>
                         <template>
-                            <div class="text-center text-muted mb-4">
-                                <small>Or sign up with credentials</small>
-                            </div>
                             <form role="form">
                                 <base-input alternative
                                             class="mb-3"
-                                            placeholder="Name"
+                                            placeholder="Owner's Full Name"
                                             addon-left-icon="ni ni-hat-3">
+                                </base-input>
+                                 <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Store Name"
+                                            addon-left-icon="ni ni-hat-3">
+                                </base-input>
+                                  <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Username"
+                                            addon-left-icon="ni ni-hat-3">
+                                </base-input>
+                                 <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Owner's Date of Birth"
+                                            addon-left-icon="ni ni-email-83">
+                                </base-input>
+                                 <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Owner's Gender"
+                                            addon-left-icon="ni ni-email-83">
                                 </base-input>
                                 <base-input alternative
                                             class="mb-3"
-                                            placeholder="Email"
+                                            placeholder="Store Email"
+                                            addon-left-icon="ni ni-email-83">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Store Phone Number"
+                                            addon-left-icon="ni ni-email-83">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Store Address"
                                             addon-left-icon="ni ni-email-83">
                                 </base-input>
                                 <base-input alternative
                                             type="password"
                                             placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open">
+                                </base-input>
+                                 <base-input alternative
+                                            type="password"
+                                            placeholder="Confirm Password"
                                             addon-left-icon="ni ni-lock-circle-open">
                                 </base-input>
                                 <div class="text-muted font-italic">
