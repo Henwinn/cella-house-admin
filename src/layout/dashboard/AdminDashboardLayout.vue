1784e7745983d516e959f6f2c35defb9efbad736
loricof23 <loricof42@gmail.com>
2021-05-28 15:51:45 +0700

    Add sidebar list approval

--- src/layout/dashboard/AdminDashboardLayout.vue
+++ src/layout/dashboard/AdminDashboardLayout.vue
@@ -3,7 +3,8 @@
     <side-bar>
       <template slot="links">
         <sidebar-link to="/admin/dashboard" :name="$t('sidebar.dashboard')" />
-        <sidebar-link to="/admin/dropship" :name="$t('dropship')"/>
+        <sidebar-link to="/admin/listApproval" :name="$t('list approval')"/>
+         <sidebar-link to="/admin/dropship" :name="$t('dropship')"/>
         <!-- <sidebar-link to="/history" :name="$t('history')" /> -->
         <!-- <sidebar-link to="/notifications" :name="$t('sidebar.notifications')"/> -->
         <sidebar-link to="/admin/profile" :name="$t('Profile')" />
