84737033962c4317ba0339af55ebf0a711ceb7ee
loricof23 <loricof42@gmail.com>
2021-06-09 16:03:05 +0700

    Axios in login (not yet finished)

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,7 @@
 <template>
     <section class="section section-shaped section-lg my-0">
         <div class="shape shape-style-1 bg-gradient-default">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+           
         </div>
         <div class="container pt-lg-md">
             <div class="row justify-content-center">
@@ -61,7 +54,11 @@
     </section>
 </template>
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+    
+};
 </script>
 <style>
 </style>
