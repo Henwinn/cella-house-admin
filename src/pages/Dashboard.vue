3c4430dd7852ebbb3ce5dabd4ff0ec2fc33b8d8e
loricof23 <loricof42@gmail.com>
2021-06-16 12:32:28 +0700

    Delete unnecessary table (old table) Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -25,7 +25,6 @@
              <table class="table is-striped is-bordered mt-2 is-fullwidth">
                <thead>
                   <tr>
-                    <th>Id</th>
                     <th>Product Name</th>
                     <th>Quantity</th>
                     <th>Price</th>
@@ -39,7 +38,6 @@
                <tbody>
 
                     <tr v-for="product in products" :key="product.id">
-                      <td>{{ product.id }}</td>
                       <td>{{ product.name }}</td>
                       <td>{{ product.qty }}</td>
                       <td>{{ product.price }}</td>
@@ -48,11 +46,10 @@
                       <td>{{ product.note }}</td>
                       <td>{{ product.status }}</td>
                       <td class="has-text-centered">
-                        <router-link
+                        <button class="button is-success"
                           :to="{ name: 'Edit', params: { id: product.id } }"
-                          class="button is-info is-small"
-                          >Edit</router-link
-                        >
+                          
+                          >Edit</button >
                         <!-- <a
                           class="button is-danger is-small"
                           @click="deleteProduct(product.id)"
@@ -63,22 +60,6 @@
                     </tr>
                 </tbody>
              </table>
-            <!-- <base-table-dashboard :data="table1.data"
-                        :columns="table1.columns"
-                        thead-classes="text-primary"
-                        >
-                        <template slot="table-row" slot-scope="props">
-                        <span v-if="props.column.field == 'details'">
-                          <button type="button" class="btn btn-primary">
-                          View Details
-                          </button>
-                        </span>
-                        <span v-else>
-                            {{props.formattedRow[props.column.field]}}
-                        </span>
-                      </template>
-            </base-table-dashboard> -->
-
           </div>
         </card>
       </div>
@@ -106,91 +87,8 @@
 
 </template>
 <script>
-// import BaseTableDashboard from '../components/BaseTableDashboard.vue';
 import axios from "axios";
-import BaseTableDashboard from '../components/BaseTableDashboard.vue';
 import BaseButton from '../components/BaseButton.vue';
-// const tableColumns = ["Item","Name","Qty","Category", "Price", "Weight","Size","City","image"] ;
-// const tableData = [
-//   {
-//     id: 1,
-//     item: "T-Shirts",
-//     name: "Jose Carillo",
-//     qty: "10",
-//     category: "Clothes",
-//     price: "20.000",
-//     weight: "10kg",
-//     size: "Medium",
-//     city: "Barcelona",
-//     image: ""
-
-//   },
-//   {
-//     id: 2,
-//     item: "T-Shirts",
-//     name: "Miguel Gallardo",
-//     qty: "3",
-//     category: "Clothes",
-//     price: "20.000",
-//     weight: "10kg",
-//     size: "Small",
-//     city: "Guadalajara",
-//     image: ""
-//   },
-//   {
-//     id: 3,
-//     name: "Antonio Escobar",
-
-//   },
-//   {
-//     id: 4,
-//     name: "Dakota Rice",
-
-//   },
-//   {
-//     id: 5,
-//     name: "Mason Porter",
-//   },
-//   {
-//     id: 6,
-//     name: "Doris Greene",
-
-//   },
-//   {
-//     id: 7,
-//     name: "Philip Chaney",
-
-//   },
-//   {
-//     id: 8,
-//     name: "Minerva Hooper",
-
-//   },
-//   {
-//     id: 9,
-//      name: "Jon Porter",
-
-//   },
-//   {
-//     id: 10,
-//     name: "Andres Smith",
-//   }
-// ];
-
-// export default {
-//   components: {
-//     BaseTableDashboard
-//   },
-//   data() {
-//     return {
-//       table1: {
-//         title: "DASHBOARD *TOKO HENWIN*",
-//         columns: [...tableColumns],
-//         data: [...tableData]
-//       }
-//     };
-//   }
-// };
 export default {
   components: { BaseButton },
     name:"productsList",
