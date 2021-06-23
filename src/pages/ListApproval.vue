aef852524adb91107d474262a70968c86fb6bd6e
Henwin <henzcl@gmail.com>
2021-06-23 15:14:19 +0700

    fix the table

--- src/pages/ListApproval.vue
+++ src/pages/ListApproval.vue
@@ -7,39 +7,33 @@
          
 
          <div class="table-responsive">
-           <h1>List Approval</h1>
+           <h1>List Approvals</h1>
           <table class="table is-striped is-bordered mt-2 is-fullwidth">
                <thead>
                   <tr>
-                    <th>Product ID</th>
+                    <th>Merchant Name</th>
+                    <th>Item Name</th>
                     <th>Qty</th>
-                    <th>Name</th>
-                    <th>Phone</th>
-                    <th>Province</th>
-                    <th>City</th>
-                    <th>Kelurahan</th>
-                    <th>Postal Code</th>
-                    <th>Address</th>
-                    <th>Shipment Price</th>
-                    <th>Status</th>
+                    <th>Price</th>
+                    <th>Category</th>
+                    <th>Variant</th>
+                    <th>Note</th>
+                   
                    
                     <th class="has-text-centered">Actions</th>
                   </tr>
                </thead>
                <tbody>
 
-                    <tr v-for="(product) in products" :key="product">
-                      <td>{{ product.productId }}</td>
+                    <tr v-for="product in products" :key="product">
+                      <td>{{ product.storeId}}</td>
+                      <td>{{ product.name }}</td>
                       <td>{{ product.qty }}</td>
-                      <td>{{ product.customerName }}</td>
-                      <td>{{ product.customerPhone }}</td>
-                      <td>{{ product.province }}</td>
-                      <td>{{ product.city }}</td>
-                      <td>{{ product.kelurahan }}</td>
-                      <td>{{ product.postalCode }}</td>
-                      <td>{{ product.address }}</td>
-                      <td>{{ product.shipmentPrice }}</td>
-                      <td>{{ product.status }}</td>
+                      <td>{{ product.price }}</td>
+                      <td>{{ product.categoryName }}</td>
+                      <td>{{ product.variant }}</td>
+                      <td>{{ product.note }}</td>
+                     
                      
                       
                       <td class="has-text-centered">
