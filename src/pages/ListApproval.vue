ab9768bf531275ddb58d37391925e934b893bc4f
Henwin <henzcl@gmail.com>
2021-06-23 12:29:51 +0700

    Update Table ListApproval

--- src/pages/ListApproval.vue
+++ src/pages/ListApproval.vue
@@ -1,14 +1,58 @@
 <template>
     <div class="row">
       <div class="col-12">
-        <card :title="table1.title">
-          <div class="table-responsive">
-            <base-table-approve :data="table1.data"
-                        :columns="table1.columns"
-                        thead-classes="text-primary"
-                        >
-            </base-table-approve>
-          </div>
+
+        
+        <card>
+         
+
+         <div class="table-responsive">
+          <table class="table is-striped is-bordered mt-2 is-fullwidth">
+               <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Item</th>
+                    <th>Qty</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                    <th>Weight</th>
+                    <th>Size</th>
+                    <th>City</th>
+                   
+                    <th class="has-text-centered">Actions</th>
+                  </tr>
+               </thead>
+               <tbody>
+
+                    <!-- <tr v-for="dropship in dropships" :key="dropship.id">
+                      <td>{{ dropship.qty }}</td>
+                      <td>{{ dropship.customerName }}</td>
+                      <td>{{ dropship.customerPhone }}</td>
+                      <td>{{ dropship.province }}</td>
+                      <td>{{ dropship.city }}</td>
+                      <td>{{ dropship.postalCode }}</td>
+                      <td>{{ dropship.address }}</td>
+                      <td>{{ dropship.shipmentPrice }}</td>
+                      <td>{{ dropship.paymentInvoice }}</td>
+                      <td>{{ dropship.note }}</td>
+                      <td>{{ dropship.status }}</td>
+                      <td class="has-text-centered">
+                        <button class="btn"
+                        
+                          >Withdraw item</button >
+                       
+                            <button class="btn">Tracking</button>
+                        
+                      </td>
+                    </tr> -->
+                </tbody>
+             </table>
+
+         </div>
+
+
+          
         </card>
       </div>
           <el-pagination
@@ -21,35 +65,8 @@
 <script>
 import BaseTableApprove from '../components/BaseTableApprove.vue';
 
-const tableColumns = ["image","Name","Item","Qty","Category", "Price", "Weight","Size","City"] ;
-const tableData= [
-  {
-    id: 1,
-    image: "",
-    name: "Toko Henwin",
-    item: "T-Shirts",
-    qty: "10",
-    category: "Clothes",
-    price: "20.000",
-    weight: "10kg",
-    size: "Medium",
-    city: "Barcelona",
-  },
-  {
-    id: 2,
-    image: "",
-    name: "Toko Sejahtera",
-    item: "T-Shirts",
-    qty: "10",
-    category: "Clothes",
-    price: "20.000",
-    weight: "10kg",
-    size: "Medium",
-    city: "Barcelona"
-  }
- 
- 
-];
+
+
 
 export default {
   components: {
@@ -58,11 +75,7 @@ export default {
   },
   data() {
     return {
-      table1: {
-        title: "List Approvals",
-        columns: [...tableColumns],
-        data: [...tableData]
-      }
+     
     };
   }
 };
