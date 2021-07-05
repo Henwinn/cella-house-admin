5bb97ee16469e368ebf7b633d5474513a69e8bf9
loricof23 <loricof42@gmail.com>
2021-07-05 12:36:32 +0700

    Changed tracking layout

--- src/pages/Tracking.vue
+++ src/pages/Tracking.vue
@@ -1,48 +1,23 @@
 <template>
     <div class="row">
       <div class="col-12">
-        <card :title="table1.title">
-          <div class="table-responsive">
-            <base-table-tracking :data="table1.data"
-                        :columns="table1.columns"
-                        thead-classes="text-primary"
-                        >
-            </base-table-tracking>
-          </div>
+        <card >
+          asd
+       
         </card>
       </div>
     </div>
 </template>
 <script>
-import BaseTableTracking from '../components/BaseTableTracking.vue';
-
-const tableColumns = ["Date","Status"] ;
-const tableData= [
-  {
-    id: 1,
-     date: "5/5/2021",
-    status: "Received",
-  },
-
- 
- 
-];
 
 export default {
-  components: {
-    BaseTableTracking
-  
-  },
+
   data() {
     return {
-      table1: {
-        title: "Tracking",
-        columns: [...tableColumns],
-        data: [...tableData]
+    
       }
-    };
+    }
   }
-};
 </script>
 <style>
 </style>
