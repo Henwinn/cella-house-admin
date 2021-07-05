ce09cf91ac59384263d81b5bd46b92b217d063e6
loricof23 <loricof42@gmail.com>
2021-07-05 11:36:55 +0700

    Fixed Add item

--- src/pages/AddFormDashboard.vue
+++ src/pages/AddFormDashboard.vue
@@ -1,137 +1,69 @@
 <template>
-    <section class="section section-shaped section-lg my-0">
-        <div class="shape shape-style-1 bg-gradient-default">
-        </div>
-        <div class="container pt-lg-md">
-            <div class="row justify-content-center">
-                <div class="col-lg-5">
-                    <card type="secondary" shadow
-                          header-classes="bg-white pb-5"
-                          body-classes="px-lg-5 py-lg-5"
-                          class="border-0">
-                        <template>
-                            <div class="text-muted text-center mb-3">
-                               Add Item
-                            </div>
-                        </template>
-                        <template>
-                            <div class="text-center text-muted mb-4">
-                                <small>Input Your Item</small>
-                            </div>
-                            <form id="app" @submit.prevent="saveItem" method="post">
-                                <p v-if="errors.length">
-                                    <b>Please correct the following error(s):</b>
-                                    <ul>
-                                        <li v-for="error in errors">{{ error }}</li> <!-- yang ini emang cacing merah ya, jangan di apa apain  -->
-                                    </ul>
-                                </p>
+    <card>
+    
+        <template>
+           <h5 slot="header" class="title">Add Item</h5>
+            <form @submit.prevent="saveItem" method="post">
+                <p v-if="errors.length">
+              <b>Please correct the following error(s):</b>
+                <ul>
+                 <li v-for="error in errors">{{ error }}</li> <!-- yang ini emang cacing merah ya, jangan di apa apain  -->
+                </ul>
+                </p>
 
-                                <div class="form-group" >
-                                    <input type="text" placeholder="Name" v-model="name" name="name" class="form-control"  />
+                                        <div class="row">
+                                <div class="col-md-5 pr-md-1">
+                                    <div class="form-group" >
+                                        <input  type="text" placeholder="Item Name" v-model="name" name="name" class="form-control"  />
+                                    </div>
                                 </div>
-
-                                  <div class="form-group" >
-                                    <input type="number" placeholder="Quantity" v-model="qty" name="qty" class="form-control"  />
+                                <div class="col-md-4 pl-md-1">
+                                    <div class="form-group" >
+                                        <input  type="text" placeholder="Quantity" v-model="qty" name="qty" class="form-control"  />
+                                    </div>
                                 </div>
-
-                                <div class="form-group" >
-                                    <input type="number" placeholder="Price" v-model="price" name="price" class="form-control"  />
+                                <div class="col-md-3 px-md-1">
+                                    <div class="form-group" >
+                                        <input  type="text" placeholder="Price" v-model="price" name="price" class="form-control"  />
+                                    </div>
                                 </div>
-
-                                <select v-model="categoryName">
-                                    <option disabled>Please Select One</option>
-                                    <option v-for="category in categories" :key="category.name" :value="category.name"> {{category.name}} </option>
-                                </select>
-
-                                 <div class="form-group" >
-                                    <input type="text" placeholder="Variant" v-model="variant" name="variant" class="form-control"  />
+                                
                                 </div>
-
-                                <div class="form-group" >
-                                    <input type="text" placeholder="Note" v-model="note" name="note" class="form-control"  />
+                                
+                                <div class="row">
+                                <div class="categories-dropdown">
+                                    <select v-model="categoryName">
+                                        <option disabled>Please Select One</option>
+                                        <option v-for="category in categories" :key="category.name" :value="category.name"> {{category.name}} </option>
+                                    </select>
+                                </div>
+                                <div class="col-md-6 pl-md-1">
+                                    <div class="form-group" >
+                                    <input  type="text" placeholder="Variant" v-model="variant" name="variant" class="form-control"  />
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group" >
+                                    <input  type="text" placeholder="Note" v-model="note" name="note" class="form-control"  />
+                                    </div>
+                                </div>
                                 </div>
 
                                  <div class="text-center">
                                     <!-- <base-button type="primary" class="my-4" @click="saveItem">Add Item</base-button> -->
                                     <button class="btn" type="submit" >    Add </button>
                                 </div>
-
-
-                                <!-- <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Name"
-                                            v-model="itemName"
-                                           >
-                                </base-input>
-                               
-                                <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Quantity"
-                                            v-model="qty"
-                                          >
-                                </base-input>
-                                 <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Price"
-                                            v-model="price"
-                                      >
-                                </base-input> -->
-                                <!-- <div class="category">
-                                    <base-dropdown title-classes="btn btn-secondary" title="Category" >
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                    </base-dropdown>
-                                </div> -->
-                                 <!-- <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Category"
-                                            v-model="category"
-                                      >
-                                </base-input> -->
-                             
-                               
-                                <!-- <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Variant"
-                                            v-model="variant"
-                                          >
-                                </base-input>
-
-
-                                 <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Note"
-                                            v-model="note"
-                                           >
-                                </base-input> -->
-                               <!-- <div class="uploadimage">
-                                    <base-input alternative
-                                                class="mb-3"
-                                                placeholder="Choose Image" >
-                                            
-                                    </base-input>
-                                    <button>Upload</button>
-                               </div> -->
-                                <!-- <div class="text-center">
-                                    <base-button type="primary" class="my-4" @click="saveItem">Add Item</base-button>
-                                    <button class="btn" type="submit" >    Add </button>
-                                </div> -->
-
-
                             </form>
                         </template>
                     </card>
-                </div>
-            </div>
-        </div>
-    </section>
+            
 </template>
 <script>
 import axios from "axios";
 export default {
     name: "AddItem",
-    el: '#app',
     data() {
         return {
             name:"",
