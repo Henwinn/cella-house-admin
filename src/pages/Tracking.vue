2fd15251f77104ce335cee3075c460f61d078141
Henwin <henzcl@gmail.com>
2021-07-09 12:38:08 +0700

    create layout tracking

--- src/pages/Tracking.vue
+++ src/pages/Tracking.vue
@@ -2,7 +2,43 @@
     <div class="row">
       <div class="col-12">
         <card >
-          asd
+          asdas
+             <!-- <div class="table-responsive">
+            <h1>Tracking</h1>
+            
+            
+             <table class="table is-striped is-bordered mt-2 is-fullwidth">
+               <thead>
+                  <tr>
+                    <th>Merchant Name</th>
+                    <th>Customer Name</th>
+                    <th>Address</th>
+                    <th>Event Set</th>
+                  </tr>
+               </thead>
+               <tbody>
+
+                    <tr v-for="(tracking, idx) in this.products" :key="idx">
+                      <td>{{ product.name }}</td>
+                      <td>{{ product.qty }}</td>
+                      <td>{{ product.price }}</td>
+                      <td>{{ product.categoryName }}</td>
+                      <td>{{ product.variant }}</td>
+                      <td>{{ product.note }}</td>
+                      <td>{{ product.status }}</td>
+                      <td class="has-text-centered">
+                       
+                         
+
+                            
+                        
+                       
+                        
+                      </td>
+                    </tr>
+                </tbody>
+             </table>
+          </div> -->
        
         </card>
       </div>
